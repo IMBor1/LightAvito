24e372626c310d5d5c653da88a1ab266e6e5f256
IMBor1 <ilacerber1@gmail.com>
2024-08-12 19:53:20 +0300

    user and repository

---
+++ src/main/java/ru/skypro/homework/model/User.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework.model;
+
+import ru.skypro.homework.dto.Role;
+
+public class User {
+    private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String currentPassword;
+    private String phone;
+    private String image;
+}
