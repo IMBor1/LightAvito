b3b7bd618a805852758670ff219bf356ff18d79f
Anton <anton22582258@gmail.com>
2024-08-18 15:39:32 +0300

    Написал документацию к контроллерам

--- src/main/java/ru/skypro/homework/controller/AdsController.java
+++ src/main/java/ru/skypro/homework/controller/AdsController.java
@@ -1,8 +1,15 @@
 package ru.skypro.homework.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.dto.ads.AdDTO;
 import ru.skypro.homework.dto.ads.AdsDTO;
@@ -17,26 +24,61 @@ import ru.skypro.homework.dto.ads.ExtendedAdDTO;
 public class AdsController {
 
     @GetMapping
+    @Operation(summary = "Получение всех объявлений")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     public ResponseEntity<AdsDTO> findAllAds() {
         return ResponseEntity.ok().build();
     }
 
     @PostMapping
+    @Operation(summary = "Добавление объявления")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "Created"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")
+    })
     public ResponseEntity<AdDTO> createAd(@RequestBody AdDTO adDTO) {
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получение информации об объявлении")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = ExtendedAdDTO.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     public ResponseEntity<ExtendedAdDTO> getAdInfo(@PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
 
+
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @adsService.isAuthorAd(principal.username, #adId)")
+
+    @Operation(summary = "Удаление объявления")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "No Content"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     public ResponseEntity deleteAd(@PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
 
     @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or @adsService.isAuthorAd(principal.username, #adId)")
+    @Operation(summary = "Обновление информации об объявлении")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = @Content(schema = @Schema(implementation = AdDTO.class))),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not found")
+    })
     public ResponseEntity<AdDTO> updateAdInfo(@PathVariable Integer id, @RequestBody CreateOrUpdateAdDTO createOrUpdateAd) {
         return ResponseEntity.ok().build();
     }
@@ -46,7 +88,8 @@ public class AdsController {
         return ResponseEntity.ok().build();
     }
 
-    @PatchMapping("/{id}/image")
+    @PatchMapping(value = "/{id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @PreAuthorize("hasRole( 'ADMIN' ) or @adServiceImpl.findAdById(id).author.userName.equals(authentication.name)")
     public ResponseEntity updateAdImage(@PathVariable Integer id) {
         return ResponseEntity.ok().build();
     }
